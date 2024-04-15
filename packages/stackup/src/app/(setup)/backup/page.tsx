37651bc7528541c50c8ef72ef5d9ab34f8883a9b
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-04-15 17:45:40 +0700

    Complete backup screen base UI

--- packages/stackup/src/app/(setup)/backup/page.tsx
+++ packages/stackup/src/app/(setup)/backup/page.tsx
@@ -10,14 +10,14 @@ import { Label } from "@/components/ui/label";
 function Page() {
     const [currentPassword, setCurrentPassword] = useState("");
     const [passwordConfirmation, setPasswordConfirmation] = useState("");
-    const step = 1;
+    const step = 2;
 
     return (
         <div>
             <div className="absolute w-full top-0 right-0">
                 <Progress
                     className="w-[99.5%]"
-                    value={step * 33}
+                    value={step * 25}
                     style={{ height: "4px" }}
                 />
             </div>
@@ -44,8 +44,8 @@ function Page() {
                     />
                 </svg>
             </Button>
-            <div className="flex full-w justify-between mt-4">
-                <div className="h2-bold text-blackleading-[38.4px] mt-4 flex">
+            <div className="flex full-w justify-between items-center mt-4">
+                <div className="h2-bold text-blackleading-[38.4px] flex">
                     Backup your account
                 </div>
                 <div className="flex items-center cursor-pointer">
@@ -68,7 +68,7 @@ function Page() {
                             fill="#867DFC"
                         />
                     </svg>
-                    <div className="text-indigo-custom b2-md">
+                    <div className="text-indigo-custom b2-regular">
                         How backup works?
                     </div>
                 </div>
@@ -80,7 +80,7 @@ function Page() {
                 to recover your account on any browser or desktop device.
             </p>
 
-            <div className="space-y-4">
+            <div className="space-y-3 my-6 px-14">
                 <div>
                     <Label htmlFor="current_password">Enter a password</Label>
                     <PasswordInput
@@ -106,15 +106,9 @@ function Page() {
                         placeholder="password"
                     />
                 </div>
-                <Button
-                    className="bg-indigo-primary hover:bg-indigo-hover active:bg-indigo-active w-full self-center mt-8"
-                    // onClick={nextPageClick}
-                >
-                    Proceed
-                </Button>
                 <div className="flex items-center">
                     <svg
-                    className="mr-1"
+                        className="mr-1"
                         xmlns="http://www.w3.org/2000/svg"
                         width="20"
                         height="20"
@@ -134,25 +128,42 @@ function Page() {
                     </div>
                 </div>
 
-                <div className="b2-regular p-2 flex rounded-lg border bg-[rgba(96,154,250,0.1)] border-[#1e55af] text-[#1567E4] my-4">
-                    <svg
-                        className="mr-1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 16 16"
-                        fill="none"
-                    >
-                        <path
-                            d="M8 1.75C4.55375 1.75 1.75 4.55375 1.75 8C1.75 11.4462 4.55375 14.25 8 14.25C11.4462 14.25 14.25 11.4462 14.25 8C14.25 4.55375 11.4462 1.75 8 1.75ZM8 4.3125C8.1607 4.3125 8.31779 4.36015 8.4514 4.44943C8.58502 4.53871 8.68916 4.6656 8.75065 4.81407C8.81215 4.96253 8.82824 5.1259 8.79689 5.28351C8.76554 5.44112 8.68815 5.58589 8.57452 5.69952C8.46089 5.81315 8.31612 5.89054 8.15851 5.92189C8.0009 5.95324 7.83753 5.93715 7.68907 5.87565C7.5406 5.81416 7.41371 5.71002 7.32443 5.5764C7.23515 5.44279 7.1875 5.2857 7.1875 5.125C7.1875 4.90951 7.2731 4.70285 7.42548 4.55048C7.57785 4.3981 7.78451 4.3125 8 4.3125ZM9.5 11.375H6.75C6.61739 11.375 6.49021 11.3223 6.39645 11.2286C6.30268 11.1348 6.25 11.0076 6.25 10.875C6.25 10.7424 6.30268 10.6152 6.39645 10.5214C6.49021 10.4277 6.61739 10.375 6.75 10.375H7.625V7.625H7.125C6.99239 7.625 6.86521 7.57232 6.77145 7.47855C6.67768 7.38479 6.625 7.25761 6.625 7.125C6.625 6.99239 6.67768 6.86521 6.77145 6.77145C6.86521 6.67768 6.99239 6.625 7.125 6.625H8.125C8.25761 6.625 8.38479 6.67768 8.47855 6.77145C8.57232 6.86521 8.625 6.99239 8.625 7.125V10.375H9.5C9.63261 10.375 9.75979 10.4277 9.85355 10.5214C9.94732 10.6152 10 10.7424 10 10.875C10 11.0076 9.94732 11.1348 9.85355 11.2286C9.75979 11.3223 9.63261 11.375 9.5 11.375Z"
-                            fill="#609AFA"
-                        />
-                    </svg>
-                    <div>
-                        Connecting your Silent Shard mobile app to your browser
-                        generates a distributed key.
+                <div className="b2-regular flex flex-col border border-[#1e55af] rounded-lg my-4 p-2 bg-[rgba(96,154,250,0.1)]">
+                    <div className="flex text-[#1567E4] mb-2">
+                        <svg
+                            className="mr-1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 16 16"
+                            fill="none"
+                        >
+                            <path
+                                d="M8 1.75C4.55375 1.75 1.75 4.55375 1.75 8C1.75 11.4462 4.55375 14.25 8 14.25C11.4462 14.25 14.25 11.4462 14.25 8C14.25 4.55375 11.4462 1.75 8 1.75ZM8 4.3125C8.1607 4.3125 8.31779 4.36015 8.4514 4.44943C8.58502 4.53871 8.68916 4.6656 8.75065 4.81407C8.81215 4.96253 8.82824 5.1259 8.79689 5.28351C8.76554 5.44112 8.68815 5.58589 8.57452 5.69952C8.46089 5.81315 8.31612 5.89054 8.15851 5.92189C8.0009 5.95324 7.83753 5.93715 7.68907 5.87565C7.5406 5.81416 7.41371 5.71002 7.32443 5.5764C7.23515 5.44279 7.1875 5.2857 7.1875 5.125C7.1875 4.90951 7.2731 4.70285 7.42548 4.55048C7.57785 4.3981 7.78451 4.3125 8 4.3125ZM9.5 11.375H6.75C6.61739 11.375 6.49021 11.3223 6.39645 11.2286C6.30268 11.1348 6.25 11.0076 6.25 10.875C6.25 10.7424 6.30268 10.6152 6.39645 10.5214C6.49021 10.4277 6.61739 10.375 6.75 10.375H7.625V7.625H7.125C6.99239 7.625 6.86521 7.57232 6.77145 7.47855C6.67768 7.38479 6.625 7.25761 6.625 7.125C6.625 6.99239 6.67768 6.86521 6.77145 6.77145C6.86521 6.67768 6.99239 6.625 7.125 6.625H8.125C8.25761 6.625 8.38479 6.67768 8.47855 6.77145C8.57232 6.86521 8.625 6.99239 8.625 7.125V10.375H9.5C9.63261 10.375 9.75979 10.4277 9.85355 10.5214C9.94732 10.6152 10 10.7424 10 10.875C10 11.0076 9.94732 11.1348 9.85355 11.2286C9.75979 11.3223 9.63261 11.375 9.5 11.375Z"
+                                fill="#609AFA"
+                            />
+                        </svg>
+                        <div>Password must contain atleast</div>
                     </div>
+                    <ul
+                        className="ml-5 text-[#609AFA]"
+                        style={{ listStyleType: "disc" }}
+                    >
+                        <li>Eight (8) characters</li>
+                        <li>One Number</li>
+                        <li>One Lower case alphabet</li>
+                        <li>One Upper case alphabet</li>
+                        <li>One special character (@,$,#,! etc)</li>
+                    </ul>
                 </div>
+            </div>
+            <div className="w-full justify-center flex">
+                <Button
+                    className="bg-indigo-primary hover:bg-indigo-hover active:bg-indigo-active w-1/2"
+                    // onClick={nextPageClick}
+                >
+                    Proceed
+                </Button>
             </div>
         </div>
     );
