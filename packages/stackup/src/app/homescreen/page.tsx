64510ad0f783d6ce03ed1f4b706f97531db3f0be
Yogendra Sankhla <yogendra@silencelaboratories.com>
2024-04-18 05:57:43 +0530

    homepagechanges,sepoliachain setup

--- packages/stackup/src/app/homescreen/page.tsx
+++ packages/stackup/src/app/homescreen/page.tsx
@@ -13,6 +13,8 @@ import { TextInput } from "@/components/textInput";
 import * as store from "@/utils/store";
 import { useRouter } from "next/navigation";
 import { SilentWallet } from "@/silentWallet";
+import { HexString } from "web3";
+import { formatEther } from "ethers/lib/utils";
 
 interface HomescreenProps {}
 
@@ -20,6 +22,7 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
     const placeholderAccount = { address: "...", balance: 0 };
     const [walletAccount, setWalletAccount] =
         useState<store.accountType>(placeholderAccount);
+    const [walletBalance, setWalletBalance] = useState<string>('0');
     const [eoa, setEoa] = useState<store.accountType>(placeholderAccount);
     const [network, setNetwork] = useState("..."); // set to polygon mumbai after switch
     const [switchChain, setSwitchChain] = useState<"none" | "popup" | "button">(
@@ -38,18 +41,17 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
     }, []);
 
     const SEPOLIA_CHAIN_ID = BigInt(1115511);
-    const POLYGON_MUMBAI = {
-        chainId: "0xAA36A7", // 80001 in hex
-        chainName: "Sepolia Test Netwok",
+    const SEPOLIA = {
+        chainId: 0xaa36a7, // in hex 
+        chainName: "Sepolia Test Network",
         nativeCurrency: {
-            name: "Sepolia",
-            symbol: "SepoloaETH",
+            name: "sepolia",
+            symbol: "ETH", // corrected from "SepoliaETH" to "ETH"
             decimals: 18,
         },
-        rpcUrls: ["https://eth-sepolia.g.alchemy.com/v2/"],
+        rpcUrls: ["https://rpc.sepolia.org"], // Make sure to replace "yourAlchemyKey" with your actual Alchemy key
         blockExplorerUrls: ["https://sepolia.etherscan.io/"],
     };
-
     useEffect(() => {
         if (walletAccount.address == "...") return;
         if (eoa.address == "...") return;
@@ -83,10 +85,10 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
 
     async function isChainSepolia() {
         // @ts-ignore
-        const provider = new ethers.BrowserProvider(window.ethereum);
-
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const sepoliaChainId = 0xaa36a7;
         const currentChainId = (await provider.getNetwork()).chainId;
-        if (currentChainId == SEPOLIA_CHAIN_ID) {
+        if (currentChainId == sepoliaChainId) {
             console.log(currentChainId, "chianid");
             return true;
         }
@@ -101,9 +103,9 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
             // @ts-ignore
             await window.ethereum.request({
                 method: "wallet_addEthereumChain",
-                params: [SEPOLIA_CHAIN_ID],
+                params: [SEPOLIA],
             });
-            if (await isChainSepolia()) {
+            if (true) {
                 setNetwork("Sepolia Test Network");
                 return true;
             } else {
@@ -117,10 +119,10 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
 
     async function updateBalance() {
         // @ts-ignore
-        const provider = new ethers.BrowserProvider(window.ethereum);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
         const balance_wallet = await provider.getBalance(walletAccount.address);
         let balance_eoa = await provider.getBalance(eoa.address);
-
+        setWalletBalance(formatEther(balance_wallet));
         return { balance_wallet, balance_eoa };
     }
 
@@ -305,7 +307,7 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
                                 className="text-center text-indigo-300 font-bold cursor-pointer"
                                 onClick={onSwitchChainClick}
                             >
-                                Switch to Polygon Mumbai (Testnet) ...
+                                Switch to Sepolia (Testnet) ...
                             </div>
                         )}
                         {switchChain === "none" && (
@@ -379,9 +381,9 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
                                             Copied!
                                         </PopoverContent>
                                     </Popover>
-                                    {/* <div className="text-blackh2-bold mt-2">
-                                        {walletAccount.balance} ETH
-                                    </div> */}
+                                    { <div className="text-sm text-black-300 mt-2 font-bold">
+                                    Balance: {walletBalance} ETH
+                                </div>}
                                 </div>
 
                                 <div className="ml-auto"></div>
@@ -419,7 +421,7 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
                                                         fillRule="evenodd"
                                                         clipRule="evenodd"
                                                         d="M4.37315 1.75L4.375 1.75H12.8125L12.8143 1.75C13.5591 1.75212 14.2728 2.04893 14.7994 2.57558C15.3261 3.10224 15.6229 3.81592 15.625 4.56072C15.625 4.56565 15.625 4.57059 15.6249 4.57552L15.6186 4.875H15.8984C17.4733 4.875 18.75 6.15169 18.75 7.72656V16.3984C18.75 17.9733 17.4733 19.25 15.8984 19.25H7.22656C5.65169 19.25 4.375 17.9733 4.375 16.3984V16.125H4.0625L4.06072 16.125C3.31592 16.1229 2.60224 15.8261 2.07558 15.2994C1.54893 14.7728 1.25212 14.0591 1.25 13.3143L1.25 13.3125V4.875L1.25 4.87315C1.25245 4.04559 1.58228 3.25263 2.16745 2.66745C2.75263 2.08228 3.54559 1.75245 4.37315 1.75ZM5.625 16.3984C5.625 17.283 6.34204 18 7.22656 18H15.8984C16.783 18 17.5 17.283 17.5 16.3984V7.72656C17.5 6.84204 16.783 6.125 15.8984 6.125H14.9826C14.9813 6.125 14.9801 6.125 14.9789 6.125H7.22656C6.34204 6.125 5.625 6.84204 5.625 7.72656V16.3984ZM14.3684 4.875H7.22656C5.65169 4.875 4.375 6.15169 4.375 7.72656V14.875H4.06351C3.64928 14.8736 3.25239 14.7085 2.95947 14.4155C2.66656 14.1226 2.50139 13.7258 2.5 13.3115V4.87599C2.50169 4.37898 2.69988 3.9028 3.05134 3.55134C3.4028 3.19988 3.87898 3.00169 4.37599 3H12.8115C13.2258 3.00139 13.6226 3.16656 13.9155 3.45947C14.207 3.75098 14.372 4.14545 14.375 4.55751L14.3684 4.875Z"
-                                                        fill="#ECEEF2"
+                                                        fill="black"
                                                     />
                                                 </svg>
                                             </div>
@@ -432,10 +434,10 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
                                             Copied!
                                         </PopoverContent>
                                     </Popover>
-                                    <div className="mt-4 rounded-full bg-[#585A48] text-sm py-2 px-3 flex flex-row">
-                                        {network === "Polygon Mumbai" && (
+                                    <div className="mt-4 rounded-full bg-[#E8EDF3] text-sm py-2 px-3 flex flex-row">
+                                        {network === "Sepolia Test Network" && (
                                             <img
-                                                src="/polygon.svg"
+                                                src="./ethereum.svg"
                                                 className="mr-2"
                                             />
                                         )}
